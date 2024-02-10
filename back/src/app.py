ea39da66e69b622fedaef6e191e39b4a53ea52fd
David Ribeiro da Silva <david.ribeiro.professional@gmail.com>
2024-02-09 21:17:51 -0300

    feat: add find all quotes function

--- back/src/app.py
+++ back/src/app.py
@@ -5,10 +5,24 @@ app = Flask(__name__)
 
 binance_service = BinanceService()
 
+@app.route('/quote',methods=['GET'])
+async def findAllExchangeInfo():
+   try:
+      result = await binance_service.findAllExchangeInfo()
+
+      data = json.dumps(result)
+
+      response = app.response_class(status=200,response=data)
+
+      return response
+   except:
+      response = app.response_class(status=400)
+
+      return response
+
 @app.route('/quote', methods=['POST'])
 async def findQuotesFromLast24Hours():
     try:
-
       body = request.json
 
       result = await binance_service.findQuotesFromLast24Hours(body['symbol'])
--- back/src/binance_service.py
+++ back/src/binance_service.py
@@ -8,10 +8,20 @@ class BinanceService:
     def __init__(self):
         self.client = Client(base_url='https://testnet.binance.vision',timeout=100)
 
+    async def findAllExchangeInfo(self):
+        try:
+            query = self.client.exchange_info()
+
+            return query
+
+        except:
+            raise Exception('Não foi possível recuperar todas as cotações')
 
     async def findQuotesFromLast24Hours(self, symbol):
         try:
             query = self.client.exchange_info(symbol=symbol)
+
             return query
+
         except:
             raise Exception('Não foi possível recuperar a cotação')
